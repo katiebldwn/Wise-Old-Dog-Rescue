1b28dfcf6a5639649efaf576aa57212c0d6b7f02
Katie Baldwin <katiebldwn@gmail.com>
2017-08-29 20:39:16 -0500

    server get

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ var app = express();
 
 
 app.use(express.static("/"));
+
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/public/index.html');
+});
+
+
+app.listen(8080, function() {
+	console.log('App running on port 8080');
+})
