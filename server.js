ef1fe750f145d452ebf93c1817087e52144e5522
Katie Baldwin <katiebldwn@gmail.com>
2017-10-05 13:22:10 -0500

    all static files to public

--- server.js
+++ server.js
@@ -2,22 +2,32 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-
-app.use(express.static('/'));
-
+app.use(express.static('public'));
 app.use(bodyParser.json());
-
-
-
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/index.html');
-});
-
-
-// app.listen(8080, function() {
-// 	console.log('App running on port 8080');
-// })
-
 app.listen(process.env.PORT || 8080, function() {
     console.log('App running');
 })
+
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+
+
+// app.use(express.static('/'));
+
+// app.use(bodyParser.json());
+
+
+
+// app.get("/", (request, response) => {
+//   response.sendFile(__dirname + '/index.html');
+// });
+
+
+// // app.listen(8080, function() {
+// // 	console.log('App running on port 8080');
+// // })
+
+// app.listen(process.env.PORT || 8080, function() {
+//     console.log('App running');
+// })
